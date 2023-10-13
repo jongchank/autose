c5f09277aea6610d329f6461a598dde9a2cb42f8
Jong-Chan Kim <jongchank@gmail.com>
2023-10-13 13:08:32 +0900

    Fix dbg.c

--- T3/dbg.c
+++ T3/dbg.c
@@ -9,20 +9,20 @@
 int main(void)
 {
     int a, b, c;
-    DBG("a = %d", a);
     a = -1;
-    DBG("b = %d", b);
+    DBG("a = %d", a);
     b = 1;
-    DBG("c = %d", c);
-    c = -1;
-    DBG("a = %d", a);
-    a = b + c;
     DBG("b = %d", b);
-    b = a + c;
+    c = -1;
     DBG("c = %d", c);
-    c = a + b + 1;
+    a = b + c;
     DBG("a = %d", a);
+    b = a + c;
+    DBG("b = %d", b);
+    c = a + b + 1;
+    DBG("c = %d", c);
     a = b / c;
+    DBG("a = %d", a);
     b = a * c;
     printf("a b c = %d %d %d\n", a, b, c);
 
